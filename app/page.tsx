49f502abb0911d0c5700e149724a5fb6bdc3d41b
Lelouch <118893915+darkzen0x@users.noreply.github.com>
2023-08-16 22:08:24 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home=()=>{
+    return(
+        <section className="flex-start flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>posts</h1>
+            <h1>loadMore</h1>
+        </section>
+    )
+}
+
+export default Home;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,16 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+
+const User = g.model('User', {name: g.string().length({min:2, max:20})})
+
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+})
